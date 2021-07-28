f1e2fe6a894b8dbd029cb0a7ad14f1c815b9f520
David Vidal <david.vidal97@gmail.com>
2021-07-27 21:04:46 -0700

    useLocalStorage Successfully added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 
 // React Components
 import NewTextForm from "./components/newTextForm";
 import TextsViewer from "./components/TextsViewer";
 
+// Custom Hooks.
+import { useLocalStorage } from "./custom/useLocalStorage";
+
 // Interfaces
 import { SingleText } from "./interfaces/interfaces";
 
-// Sample
-import texts from "./samples/texts";
+// Samples for testings
+//import texts from "./samples/texts";
+
+//import textsJson from "./samples/textsJson.json"
+//const pruebita: SingleText = textsJson
+//console.log(pruebita)
+
 
 const App = (): JSX.Element => {
 
-  const [textsList, setTextList] = useState<SingleText[]>(texts);
+  const [textsList, setTextList] = useLocalStorage("textsList",[]);
+
+  //const [listPrueba, setListPrueba] = useLocalStorage("prueba", []);
+
 
   // NewTextForm Functions.
   // -----------------------------------------------
@@ -21,7 +32,23 @@ const App = (): JSX.Element => {
 
     const newTextList: SingleText[] = [...textsList, addedText];
     setTextList(newTextList);
+
+    //const prueba:string[] = [...listPrueba, addedText.title];
+    //setListPrueba(prueba);
+
   };
+
+
+
+
+  /*
+  const button = () =>{
+    console.log(listPrueba)
+  }
+
+  Add this to the return function:
+      <button onClick={button}>click</button>
+  */
 
   
   // -----------------------------------------------
@@ -35,7 +62,6 @@ const App = (): JSX.Element => {
     <Fragment>
       <NewTextForm addingText={addingText} />
       <TextsViewer textsList={textsList}/>
-
     </Fragment>
   );
 };
