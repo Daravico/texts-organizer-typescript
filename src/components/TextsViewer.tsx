a41dfb83de2f117b124c857e06a242cdcbfa622e
David Vidal <david.vidal97@gmail.com>
2021-08-04 23:33:22 -0700

    Post Cancellation

--- src/components/TextsViewer.tsx
+++ src/components/TextsViewer.tsx
@@ -26,48 +26,6 @@ const TextsViewer: React.FC<ViewerProps> = ({ textsList }) => {
     const [textsListFiltered, setTextsListFiltered] = useState<SingleText[]>(textsList);
 
 
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-    // QUERY FILTERING DEPENDING ON THE SEARCH BAR.
-    // The Method to filter depending on the query should be here to re-render on different situations:
-    // On changes made on the Search Bar, edditing of the selected text or deletion of this.
-/*
-     // Query that filters the texts list depending on the value on the search bar changes.
-     const queryFilter = (selectBoxRef: React.MutableRefObject<HTMLSelectElement | null>, searchBarRef: React.MutableRefObject<HTMLInputElement>) => {
-
-        // When the search bar is empty.
-        if (searchBarRef.current?.value === '') {
-            setTextsListFiltered(textsList);
-        }
-
-        // Search bar with a character. Right now it works only with title <<TO ADD: SELECTION BETWEEN TITLE, CATEGORY, TAGS* AND MIXED>>
-        else {
-            const filterAplied: SingleText[] = textsList.filter((text) => {
-
-                // To lower cases to simplify the search.
-                const queryLower: string = searchBarRef.current.value.toLowerCase();
-
-                const titleLower: string = text.title.toLowerCase();
-                const categoryLower: string = text.category.toLowerCase();
-                const textLower: string = text.text.toLowerCase();
-
-                if (selectBoxRef.current?.value === 'Title')
-                    return titleLower.includes(queryLower);
-                if (selectBoxRef.current?.value === 'Category')
-                    return categoryLower.includes(queryLower);
-                if (selectBoxRef.current?.value === 'Text')
-                    return textLower.includes(queryLower);
-
-                else 
-                    return titleLower.includes(queryLower) || categoryLower.includes(queryLower) || textLower.includes(queryLower);
-            });
-
-            // Setting the state with the applied filter.
-            setTextsListFiltered(filterAplied);
-        }
-    }
-    */
-// -------------------------------------------------------------------------------------------------------------------------------------------------
-
 
     return (
         <CardGroup>
